ea855fab752b1ab3b271f781489f335eef5523c1
kasper_askar <askar.baygenzhin@gmail.com>
2021-09-30 14:54:43 +0600

    first commit

---
+++ week2/stringers.go
@@ -0,0 +1,27 @@
+  
+package main
+
+import (
+	"fmt" 
+	"strings"
+)
+
+type IPAddr [4]byte
+
+func (ip IPAddr) String() string {
+	s := make([]string, len(ip))
+	for i, v := range ip {
+		s[i] = fmt.Sprintf("%d", v)
+	}
+	return strings.Join(s, ".")
+}
+
+func main() {
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
